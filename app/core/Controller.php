f423bb80c6eb8b82b665de4221c15bc1673a8ce6
yufi <yufi.gitlab@gmail.com>
2020-06-28 05:54:40 +0700

    Menambahkan beberapa folder

---
+++ app/core/Controller.php
@@ -0,0 +1,9 @@
+<?php 
+
+// class controller ini meruakan kelas utama.
+// sedangkan class-class yg kita buat di folder controllers adalah controller yg akan extend ke class Controller
+class Controller {
+  
+}
+
+?>
